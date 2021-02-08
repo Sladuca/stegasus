78523fb730378a07fb63692fac161601fb70883f
Sebastien La Duca <sladuca777@gmail.com>
2021-02-07 21:16:48 -0500

    add image output to test

--- src/lib.rs
+++ src/lib.rs
@@ -170,32 +170,41 @@ mod tests {
     use std::fs::File;
     use std::io::prelude::*;
 
-    fn test_sporkmarmot(data: &[u8]) {
+    fn test_sporkmarmot(data: &[u8], output_suffix: Option<&'static str>) {
         let mut buf = Vec::new();
-        let mut f = File::open("./pkg/examples/sporkmarmot_riding_bufficorn.png").unwrap();
+        let mut f = File::open("./sporkmarmot_riding_bufficorn.png").unwrap();
         f.read_to_end(&mut buf).unwrap();
 
         let steg = encode_img_inner(&buf, data).unwrap();
         let decoded = decode_img_inner(&steg).unwrap();
+        if let Some(output_suffix) = output_suffix {
+            let mut f = File::create(format!(
+                "./sporkmarmot_riding_bufficorn_steg_{}.png",
+                output_suffix
+            ))
+            .unwrap();
+
+            f.write_all(&steg).unwrap();
+        }
 
         assert_eq!(&data[..], decoded);
     }
 
     #[test]
     fn test_hello_world() {
-        test_sporkmarmot(b"hello world!");
+        test_sporkmarmot(b"hello world!", Some("hello_world"));
     }
 
     #[test]
     fn test_various_lengths() {
         let data = b"my hovercraft is full of eels!";
-        test_sporkmarmot(&data[..]);
+        test_sporkmarmot(&data[..], None);
 
         let data = b"Victorious warriors win first and then go to war, while defeated warriors go \
         to war first and then seek to win";
-        test_sporkmarmot(&data[..]);
+        test_sporkmarmot(&data[..], None);
 
         let data = b"Governments of the Industrial World, you weary giants of flesh and steel, I come from Cyberspace, the new home of Mind. On behalf of the future, I ask you of the past to leave us alone. You are not welcome among us. You have no sovereignty where we gather.\r\n\r\nWe have no elected government, nor are we likely to have one, so I address you with no greater authority than that with which liberty itself always speaks. I declare the global social space we are building to be naturally independent of the tyrannies you seek to impose on us. You have no moral right to rule us nor do you possess any methods of enforcement we have true reason to fear.\r\n\r\nGovernments derive their just powers from the consent of the governed. You have neither solicited nor received ours. We did not invite you. You do not know us, nor do you know our world. Cyberspace does not lie within your borders. Do not think that you can build it, as though it were a public construction project. You cannot. It is an act of nature and it grows itself through our collective actions.\r\n\r\nYou have not engaged in our great and gathering conversation, nor did you create the wealth of our marketplaces. You do not know our culture, our ethics, or the unwritten codes that already provide our society more order than could be obtained by any of your impositions.\r\n\r\nYou claim there are problems among us that you need to solve. You use this claim as an excuse to invade our precincts. Many of these problems don\'t exist. Where there are real conflicts, where there are wrongs, we will identify them and address them by our means. We are forming our own Social Contract. This governance will arise according to the conditions of our world, not yours. Our world is different.\r\n\r\nCyberspace consists of transactions, relationships, and thought itself, arrayed like a standing wave in the web of our communications. Ours is a world that is both everywhere and nowhere, but it is not where bodies live.\r\n\r\nWe are creating a world that all may enter without privilege or prejudice accorded by race, economic power, military force, or station of birth.\r\n\r\nWe are creating a world where anyone, anywhere may express his or her beliefs, no matter how singular, without fear of being coerced into silence or conformity.\r\n\r\nYour legal concepts of property, expression, identity, movement, and context do not apply to us. They are all based on matter, and there is no matter here.\r\n\r\nOur identities have no bodies, so, unlike you, we cannot obtain order by physical coercion. We believe that from ethics, enlightened self-interest, and the commonweal, our governance will emerge. Our identities may be distributed across many of your jurisdictions. The only law that all our constituent cultures would generally recognize is the Golden Rule. We hope we will be able to build our particular solutions on that basis. But we cannot accept the solutions you are attempting to impose.\r\n\r\nIn the United States, you have today created a law, the Telecommunications Reform Act, which repudiates your own Constitution and insults the dreams of Jefferson, Washington, Mill, Madison, DeToqueville, and Brandeis. These dreams must now be born anew in us.\r\n\r\nYou are terrified of your own children, since they are natives in a world where you will always be immigrants. Because you fear them, you entrust your bureaucracies with the parental responsibilities you are too cowardly to confront yourselves. In our world, all the sentiments and expressions of humanity, from the debasing to the angelic, are parts of a seamless whole, the global conversation of bits. We cannot separate the air that chokes from the air upon which wings beat.\r\n\r\nIn China, Germany, France, Russia, Singapore, Italy and the United States, you are trying to ward off the virus of liberty by erecting guard posts at the frontiers of Cyberspace. These may keep out the contagion for a small time, but they will not work in a world that will soon be blanketed in bit-bearing media.\r\n\r\nYour increasingly obsolete information industries would perpetuate themselves by proposing laws, in America and elsewhere, that claim to own speech itself throughout the world. These laws would declare ideas to be another industrial product, no more noble than pig iron. In our world, whatever the human mind may create can be reproduced and distributed infinitely at no cost. The global conveyance of thought no longer requires your factories to accomplish.\r\n\r\nThese increasingly hostile and colonial measures place us in the same position as those previous lovers of freedom and self-determination who had to reject the authorities of distant, uninformed powers. We must declare our virtual selves immune to your sovereignty, even as we continue to consent to your rule over our bodies. We will spread ourselves across the Planet so that no one can arrest our thoughts.\r\n\r\nWe will create a civilization of the Mind in Cyberspace. May it be more humane and fair than the world your governments have made before.";
-        test_sporkmarmot(&data[..]);
+        test_sporkmarmot(&data[..], None);
     }
 }
